4376dac7091aac139171087ffb53defc7c9b1344
Chang-Young <par22019@byui.edu>
2023-01-10 22:43:11 +0900

    Prep5 Changed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = GetUserName();
+        int userNum = GetUserNumber();
+        int squareNum = GetSquareNumber(userNum);
+        DisplayResult(userName, squareNum);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int GetUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userVal = Console.ReadLine();
+        int userNum = int.Parse(userVal);
+        return userNum;
+    }
+  
+    static int GetSquareNumber(int userNum)
+    {
+        userNum = userNum * userNum;
+        return userNum;
+    }   
+
+    static void DisplayResult(string userName, int squareNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareNum}");
+    }
+
 }
